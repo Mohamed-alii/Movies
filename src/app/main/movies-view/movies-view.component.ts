cfbb2b28fc7c1a5f9bdb9353e2387b11599a60cb
Mohamed-alii <36523881+Mohamed-alii@users.noreply.github.com>
2021-05-08 21:17:03 +0200

    changing food-service requests to specific page number

--- src/app/main/movies-view/movies-view.component.ts
+++ src/app/main/movies-view/movies-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MoviesService } from '../../services/movies.service'
 @Component({
   selector: 'app-movies-view',
   templateUrl: './movies-view.component.html',
@@ -7,7 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesViewComponent implements OnInit {
 
-  constructor() { }
+  popularMovies;
+  NowPlayingMovies:Array<any> = [];
+  upComingMovies;
+  // all should be in one array switch 1 2 3 movie array = depend on user click on radio button and subscribe
+  imgPrefix = this._MoviesService.imgPrefix;
+
+  constructor(private _MoviesService:MoviesService) {
+
+    // getting popular movies
+    for(let i = 1; i < 5; i++){
+
+      this._MoviesService.getPopularMovies(i).subscribe( (data) => {
+  
+        this.popularMovies = this.popularMovies.concat( data.results ) ;
+  
+      } , (err) => {} )
+
+    }
+
+    // getting latest movies
+    for(let i = 1; i < 5; i++){
+
+      this._MoviesService.getUpComingMovies(i).subscribe( (data) => {
+  
+        this.upComingMovies = this.upComingMovies.concat( data.results ) ;
+  
+      } , (err) => {} )
+
+    }
+
+    // getting now playing movies
+    for(let i = 1; i < 5; i++){
+
+      this._MoviesService.getNowPlayingMovies(i).subscribe( (data) => {
+  
+        this.NowPlayingMovies = this.NowPlayingMovies.concat( data.results ) ;
+  
+      } , (err) => {} )
+
+    }
+    
+    // end of constructor
+   }
 
   ngOnInit(): void {
   }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
+  imgPrefix: string = "https://image.tmdb.org/t/p/w500";
+  
   constructor(private Http: HttpClient) { }
 
 
-  getPopularMovies() {
-    return this.Http.get('https://api.themoviedb.org/3/trending/movie/week?api_key=6f24f3712f6a267833075d28fa873be3')
-}
+  getPopularMovies(pageNumber):Observable<any>
+  {
+    return this.Http.get(`https://api.themoviedb.org/3/trending/movie/week?page=${pageNumber}&api_key=6f24f3712f6a267833075d28fa873be3`)
+  }
+
+  
+  getUpComingMovies(pageNumber):Observable<any>
+  {
+    return this.Http.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=6f24f3712f6a267833075d28fa873be3&language=en-US&page=${pageNumber}`)
+  }
+
+  getNowPlayingMovies(pageNumber):Observable<any>
+  {
+    return this.Http.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=6f24f3712f6a267833075d28fa873be3&language=en-US&page=${pageNumber}`)
+  }
 
 }
